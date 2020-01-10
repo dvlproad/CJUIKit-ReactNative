e02119626a89f9cf346e870e9789f432df11f24b
dvlproad <dvlproad@163.com>
2020-01-10 14:42:59 +0800

    【Feature】更改库名

--- cjrn-demo-base/Default/CJTSDefault.js
+++ cjrn-demo-base/Default/CJTSDefault.js
@@ -12,5 +12,8 @@
 export const CJTSDefaultImages = {
     localImageSource1: require('./img/1.jpg'),
     localImageSource2: require('./img/4.jpg'),
-    networkImageSource1:  {uri: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3460118221,780234760&fm=26&gp=0.jpg'},
+    networkImageSource1:    {uri: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3460118221,780234760&fm=26&gp=0.jpg'},
+    networkImageCar:        {uri: 'https://timgsa.baidu.com/timg?image&quality=80&size=b10000_10000&sec=1562053677&di=9c7e86f3099929712b5f97b94bc153c5&src=http://01.minipic.eastday.com/20161109/20161109230347_7e6b8aeb814b27aad394cc65d42237d8_4.jpeg'},
+    networkImageLandscape:  {uri: 'https://h1.ioliu.cn//bing/Transfagarasan_ZH-CN5760731327_1920x1080.jpg'},
+    networkImageMountainroad:   {uri: 'https://h1.ioliu.cn//bing/Transfagarasan_ZH-CN5760731327_1920x1080.jpg'},
 };
